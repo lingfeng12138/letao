ed5b31d843e8348c72253e12d0c6eaeebb73c06e
Feng Ling <lingfeng5438@163.com>
2019-01-05 21:06:01 +0800

    用户管理完成

---
+++ public/back/js/user.js
@@ -0,0 +1,39 @@
+$(function(){
+    var currentId;
+    var idDelete;
+
+    var currentPage = 1;
+    var pageSize = 5;
+    render();
+
+    function render(){
+        $.ajax({
+        type: "get",
+        url: "/user/queryUser",
+        data:{
+            page: currentPage,
+            pageSize: pageSize
+        },
+        dataType:"json",
+        success:function(info){
+            var htmlStr = template("tpl",info);
+            $('tbody').html(htmlStr);
+
+            $('#paginator').bootstrapPaginator({
+                bootstrapMajorVersion: 3,
+                currentPage:info.page,
+                totalPages:Math.ceil(info.total/info.size),
+
+                onPageClicked: function(a,b,c,page){
+                    currentPage = page;
+
+                    render();
+                }
+            })
+        } 
+    })
+    }
+
+    
+    
+})
